b0747cdfa83aa66988ac9af577fbe55c46d55c99
Pankaj Singh <pankajsingh.nitb@gmail.com>
2022-07-26 23:33:42 +0530

    First Commit

---
+++ src/main/java/com/sipankaj/swaggerex/SwaggerGeneratorExample/SwaggerGeneratorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sipankaj.swaggerex.SwaggerGeneratorExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerGeneratorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerGeneratorExampleApplication.class, args);
+	}
+
+}
